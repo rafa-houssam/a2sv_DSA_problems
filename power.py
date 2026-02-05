ac46fb2e11feb0d76b63e7e6d137de1c5f1a0ac3
rafa-houssam <rafahoussam4edu@gmail.com>
2026-02-05 17:54:51 +0100

    add power calculation script with modular output

---
+++ power.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a=int(input().strip())
+b=int(input().strip())
+m=int(input().strip())
+power=a**b
+print(power)
+print(power%m)
