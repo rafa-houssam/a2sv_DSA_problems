e328086eebd1108e060cd60c83732d1c34ee5b54
rafa-houssam <rafahoussam4edu@gmail.com>
2026-02-13 21:02:03 +0100

    add implementation of rotateLeft function for array rotation

---
+++ hackerrankrotate.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'rotateLeft' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER d
+#  2. INTEGER_ARRAY arr
+#
+
+def rotateLeft(d, arr):
+    arr2=[0]*len(arr)
+    for i in range(len(arr)):
+        newIndex=i-d
+        if(newIndex<0):
+            newIndex+=len(arr)
+        arr2[newIndex]=arr[i]
+    return arr2
+    
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = rotateLeft(d, arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
