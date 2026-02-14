2cd59e7c4f0949a36e860dd0e0b5fc13eaceef1e
rafa-houssam <rafahoussam4edu@gmail.com>
2026-02-14 17:35:06 +0100

    refactor implementation of beautiful matrix problem for clarity and correctness

--- beatifulMatrix.py
+++ beatifulMatrix.py
@@ -1,9 +1,15 @@
-import numpy as np
 
-matrix=np.zeros((5,5),dtype=int)
+matrix = []
 for i in range(5):
-    row=input().split()
-    matrix[i]=row
-row,col=np.where(matrix==1)
-moves=abs(2-row[0])+abs(2-col[0])
-print(moves)
+    row = list(map(int, input().split()))
+    matrix.append(row)
+
+for i in range(5):
+    for j in range(5):
+        if matrix[i][j] == 1:
+            moves = abs(2 - i) + abs(2 - j)
+            print(moves)
+            break
+    else:
+        continue
+    break
